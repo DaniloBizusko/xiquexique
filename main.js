320b751256f1b306c94ae1ad214185dc4c051bb7
Danilo Bizusko <142445820+DaniloBizusko@users.noreply.github.com>
2024-09-23 10:13:35 -0300

    wgr

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function areaQuadrado(lado){
     return lado**2;
 }
 console.log(areaQuadrado(5));
-let areaTriangulo=function(){
-    return(4*3);
+let areaTriangulo=function(base,altura){
+    return(base,altura);
 }
 console.log(areaTriangulo(4*3))
