f4e720df2e5988a0903b2d51197ac29b62f2c679
Danilo Bizusko <142445820+DaniloBizusko@users.noreply.github.com>
2024-09-23 10:49:26 -0300

    jh

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ let valor1=parseInt(prompt('digite um numero'));
 let valor2=parseInt(prompt('digite um segundo numero'));
 let valor3=parseInt(prompt('digite um terceiro numero'));
 function media(valor1,valor2,valor3){
-    return (valor1*valor2*valor3)/3;
+    return (valor1+valor2+valor3)/3;
 }
 console.log(media(valor1,valor2,valor3));
 document.write(media(valor1,valor2,valor3));
