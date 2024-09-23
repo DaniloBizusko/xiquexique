ce66e8efdb57376cb21ba4d216b1f0e3f0b08a11
Danilo Bizusko <142445820+DaniloBizusko@users.noreply.github.com>
2024-09-23 10:11:30 -0300

    nty

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function areaQuadrado(lado){
     return lado**2;
 }
 console.log(areaQuadrado(5));
-let areaTriangulo=function(4*3){
+let areaTriangulo=function(){
     return(4*3);
 }
 console.log(areaTriangulo(4*3))
