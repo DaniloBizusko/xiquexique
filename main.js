368c57fb315b88c59e139f049db1494b2e17b65f
Danilo Bizusko <142445820+DaniloBizusko@users.noreply.github.com>
2024-09-30 09:15:35 -0300

    bfd

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
- alert('danilo')
-function soma(num1,num2){
-    return num1+num2;
-}
-console.log(soma(2,3));
+//  alert('danilo')
+// function soma(num1,num2){
+//     return num1+num2;
+// }
+// console.log(soma(2,3));
 
-function areaQuadrado(lado){
-    return lado**2;
-}
-console.log(areaQuadrado(5));
+// function areaQuadrado(lado){
+//     return lado**2;
+// }
+// console.log(areaQuadrado(5));
 
-function areatriangulo(base,altura){
-    return (base*altura)/2;
-}
-console.log(areatriangulo(8,5));
+// function areatriangulo(base,altura){
+//     return (base*altura)/2;
+// }
+// console.log(areatriangulo(8,5));
 
-let valor1=parseInt(prompt('digite um numero'));
-let valor2=parseInt(prompt('digite um segundo numero'));
-let valor3=parseInt(prompt('digite um terceiro numero'));
-function media(valor1,valor2,valor3){
-    return (valor1+valor2+valor3)/3;
-}
-console.log(media(valor1,valor2,valor3));
-document.write(media(valor1,valor2,valor3));
+// let valor1=parseInt(prompt('digite um numero'));
+// let valor2=parseInt(prompt('digite um segundo numero'));
+// let valor3=parseInt(prompt('digite um terceiro numero'));
+// function media(valor1,valor2,valor3){
+//     return (valor1+valor2+valor3)/3;
+// }
+// console.log(media(valor1,valor2,valor3));
+// document.write(media(valor1,valor2,valor3));
