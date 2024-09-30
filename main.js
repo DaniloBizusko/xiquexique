9bc0d27bcc0085e0ffec3130eb1aacb9c40b1c58
Danilo Bizusko <142445820+DaniloBizusko@users.noreply.github.com>
2024-09-30 10:19:46 -0300

    fh

--- main.js
+++ main.js
@@ -23,3 +23,7 @@
 // console.log(media(valor1,valor2,valor3));
 // document.write(media(valor1,valor2,valor3));
 let estados=['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito   Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná',   'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
+let;=0;
+while(i<10){
+    console.log(i);
+    i=i+1;
